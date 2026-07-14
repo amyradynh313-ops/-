cff3bf0b3759511f62fb6a4b878076834636ceba
amyradynh313-ops <amyradynh313@gmail.com>
2026-07-14 15:23:26 +0330

    Add files via upload

---
+++ db.js
@@ -0,0 +1,13 @@
+// دیتابیس ساده - فایل JSON برای ذخیره کاربرها، گفتگوها و پیام‌ها
+// (برای پروژه‌های کوچیک کافیه، نیاز به نصب دیتابیس واقعی نیست)
+
+const low = require('lowdb');
+const FileSync = require('lowdb/FileSync');
+const path = require('path');
+
+const adapter = new FileSync(path.join(__dirname, 'data.json'));
+const db = low(adapter);
+
+db.defaults({ users: [], conversations: [], messages: [] }).write();
+
+module.exports = db;
